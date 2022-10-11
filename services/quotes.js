55ebe14a1acda79ed102c0dd6eaf3ceb29ac5405
Roberth O <roberthalexander13@gmail.com>
2022-10-10 22:37:44 -0400

    Update precios

--- services/quotes.js
+++ services/quotes.js
@@ -39,6 +39,7 @@ async function getQuote(id){
 async function getTest(data){
   let resp = [];
   let _prom = 0;
+  let _version = data.version;
   await getContent(data.url)
   .then(content => {    
     let datas = content.html;
@@ -49,43 +50,52 @@ async function getTest(data){
       let _items = datas.querySelectorAll(".ui-search-result__content-wrapper");
       _items.forEach(element => {
         let _precio = element.querySelector(".ui-search-price__second-line .price-tag-fraction").textContent;
-
+        let title = element.querySelector(".shops__item-title").textContent;
+        let title2 = title.split(" ");
         let _pp = parseInt(_precio.replace(/\./g, ''));
-        _prom += _pp;
-
-        resp.push(_pp);
+        
+        if(_version.includes(title2[2])){
+          _prom += _pp;
+          resp.push({
+            title: title,
+            precop: _precio
+          });
+        }
+        
       });
-      resp.sort(function(a, b) {
-        return a - b;
-      });
+      // resp.sort(function(a, b) {
+      //   return a - b;
+      // });
     }
   })
   .catch(error => {
     console.error(error)
     process.exit(1)
   });
-  if(resp.length > 0){
-    //Si OLX no dio resultados
-    if(!data.data_olx){
-      if(resp.length > 3){
-        let _base = (resp[0] + resp [1] + resp[2]) / 3;
-        _base = _base + (_base * 0.15);
-        let resp2 = [];
-        resp.forEach(val =>{
-          if(val <= _base)
-            resp2.push(val);
-        });
-        let _total = resp2.reduce((a, b) => a + b, 0);
-        console.log(_total);
-        _prom = _total / resp2.length;
-        resp = resp2;
-      }else{
-        _prom = _prom / (resp.length);
-      }      
-    }else{
-      _prom = _prom / (resp.length);
-    }    
-  }
+  // if(resp.length > 0){
+  //   //Si OLX no dio resultados
+  //   if(!data.data_olx){
+  //     if(resp.length > 3){
+  //       let _base = (resp[0] + resp [1] + resp[2]) / 3;
+  //       _base = _base + (_base * 0.10);
+  //       let resp2 = [];
+  //       resp.forEach(val =>{
+  //         if(val <= _base)
+  //           resp2.push(val);
+  //       });
+  //       let _total = resp2.reduce((a, b) => a + b, 0);
+  //       console.log(_total);
+  //       _prom = _total / resp2.length;
+  //       resp = resp2;
+  //     }else{
+  //       _prom = _prom / (resp.length);
+  //     }      
+  //   }else{
+  //     _prom = _prom / (resp.length);
+  //   }    
+  // }
+  if(resp.length > 0)
+    _prom = _prom / resp.length;
   return { data: resp, count: resp.length, prom: Math.round(_prom) }
 }
 
