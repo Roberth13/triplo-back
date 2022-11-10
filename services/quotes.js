d588953fcffdba8ce924a0057beefaa507f15577
Roberth Ortiz <roberthalexander13@gmail.com>
2022-11-10 09:04:12 -0400

    Ajuste en precios

--- services/quotes.js
+++ services/quotes.js
@@ -51,7 +51,8 @@ async function getTest(data){
     let datas = content.html;
     const root = HTMLParser.parse(datas);
     datas = root.querySelector(".ui-search-results");
-    
+    let dats2 = root.querySelector(".ui-search-search-result");
+
     if(datas != null){      
       let _items = datas.querySelectorAll(".ui-search-result__content-wrapper");
       _items.forEach(element => {
@@ -65,6 +66,10 @@ async function getTest(data){
           price: _pp
         });
       });
+      //Cantidad de resultados
+      _items = dats2.querySelector(".ui-search-search-result__quantity-results").textContent;
+      let cant_records = _items.split("")[0];
+      console.log(cant_records);
     }
   })
   .catch(error => {
@@ -72,29 +77,44 @@ async function getTest(data){
     process.exit(1)
   });
 
-  //Empezamos a comparar
-  if(_keys.length > 0){
-    _keys.forEach(_key => {
-      if(_key !== "all"){
-        if(_key.startsWith("-")){
-          let _key1 = _key.slice(1);  
-          resp = resp.filter(x => !x.title.toLowerCase().includes(_key1.toLowerCase()))
-        }else{
-          resp = resp.filter(x => x.title.toLowerCase().includes(_key.toLowerCase()))
-        }
-      }
-    });
-  }
- 
-  resp.forEach(rr =>{
-    _prom += rr.price;
-  })
-
-  if(resp.length > 0)
+  let _cont = 0;
+  if(resp.length > 1){
+    resp.forEach(rr =>{
+      _prom += rr.price;
+    })
     _prom = _prom / resp.length;
+    let _min = _prom - (_prom * 0.25);
+    let _max = _prom + (_prom * 0.20);
+
+
+    //Empezamos a comparar
+    if(_keys.length > 0){
+      _keys.forEach(_key => {
+        if(_key !== "all"){
+          if(_key.startsWith("-")){
+            let _key1 = _key.slice(1);  
+            resp = resp.filter(x => !x.title.toLowerCase().includes(_key1.toLowerCase()))
+          }else{
+            resp = resp.filter(x => x.title.toLowerCase().includes(_key.toLowerCase()))
+          }
+        }
+      });
+    }
+ 
+    _prom = 0;    
+    resp.forEach(rr =>{
+      if(rr.price >= _min && rr.price <= _max){
+        _prom += rr.price;
+        _cont++;
+      }
+    })
+
+    if(_cont > 0)
+      _prom = _prom / _cont;
+  }
   
   
-  return { data: resp, count: resp.length, prom: Math.round(_prom) }
+  return { cant: _cont, prom: Math.round(_prom) }
 }
 
 module.exports = {
